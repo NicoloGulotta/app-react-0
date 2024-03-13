4956baf19fb0eafdd9c798527af203e310fd7df6
Nicolò Gulotta <Gulottanico1998@gmail.com>
2024-03-13 16:58:10 +0100

    Agg

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,24 +1,22 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const MyNav = () => (
-  <Navbar
-    expand="lg"
-    cla-bs-theme="dark"
-  >
-    <ContainssName="bg-body-tertiary mb-3"
-    bg="dark"
-    dataer fluid>
-      <Navbar.Brand href="#">Books</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#">Home</Nav.Link>
-          <Nav.Link href="#">About</Nav.Link>
-          <Nav.Link href="#">Browse</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-)
-
-export default MyNav
+function MyNav() {
+  return (
+    <Navbar expand="lg" className="bg-dark">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className="text-light">Home</Nav.Link>
+            <Nav.Link href="#link" className="text-light">About</Nav.Link>
+            <Nav.Link href="#link" className="text-light">Browse</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+export default MyNav;
